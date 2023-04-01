b27b545a9dd117cec8f850854e1415c9fafc9c03
Jasper Tucker <jaspermtucker@gmail.com>
2023-04-01 17:12:16 -0400

    initial collectable/instrument

--- Source/AOI_Project/BasePlayer.cpp
+++ Source/AOI_Project/BasePlayer.cpp
@@ -28,6 +28,12 @@ void ABasePlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	BoxCollision = FindComponentByClass<UBoxComponent>();
+	if (BoxCollision != nullptr)
+	{
+		BoxCollision->OnComponentBeginOverlap.AddDynamic(this, & ABasePlayer::OnOverlapBegin);
+		BoxCollision->OnComponentEndOverlap.AddDynamic(this, &ABasePlayer::OnOverlapEnd);
+	}
 }
 
 // Called every frame
@@ -45,7 +51,7 @@ void ABasePlayer::MoveForward(float InputValue)
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
 
-		// Add movement input
+		// add movement input
 		AddMovementInput(Direction, InputValue);
 
 	}
@@ -59,7 +65,7 @@ void ABasePlayer::MoveRight(float InputValue)
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 
-		// Add movement input
+		// add movement input
 		AddMovementInput(Direction, InputValue);
 
 	}
@@ -73,16 +79,49 @@ void ABasePlayer::CameraLookUp(float InputValue)
 void ABasePlayer::CameraLookRight(float InputValue)
 {
 	AddControllerYawInput(InputValue * CameraTurnRate);
-}   
+}
+
+void ABasePlayer::OnOverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (OtherActor != nullptr && OtherActor->ActorHasTag("Instrument"))
+	{
+		CurrentSelectableInstrument = (ACollectable*)OtherActor;
+	}
+}
+
+void ABasePlayer::OnOverlapEnd(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+	if (OtherActor != nullptr && OtherActor->ActorHasTag("Instrument"))
+	{
+		CurrentSelectableInstrument = nullptr;
+	}
+}
+
+void ABasePlayer::UseOrSetInstrument()
+{
+	// if player currently has an instrument, use it
+	// access methods from CurrentUsableInstrument
+
+	// if player doesn't currently have an instrument, try to pick one up
+	if (CurrentSelectableInstrument != nullptr)
+	{
+		// may want to change this to a type later, enum?
+		CurrentUsableInstrument = CurrentSelectableInstrument;
+		CurrentUsableInstrument->FindComponentByClass<UStaticMeshComponent>()->ToggleVisibility();
+		UE_LOG(LogTemp, Warning, TEXT("Picked up instrument"));
+	}
+}
+
 
 // Called to bind functionality to input
 void ABasePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
-	// bind to action mappings
+	// bind action mappings
 	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
 	PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+	PlayerInputComponent->BindAction("Interact", IE_Pressed, this, &ABasePlayer::UseOrSetInstrument);
 
 	// bind axis mappings
 	PlayerInputComponent->BindAxis("MoveForward", this, &ABasePlayer::MoveForward);
