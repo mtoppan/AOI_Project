adb7a9f8cccfc0779323e96ffa7c944f40c3d0f0
Jasper Tucker <jaspermtucker@gmail.com>
2023-04-01 14:32:36 -0400

    Initial setup for character and map + moving things around

---
+++ Source/AOI_Project/BasePlayer.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "BasePlayer.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "Camera/CameraComponent.h"
+
+// Sets default values
+ABasePlayer::ABasePlayer()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	// Create spring arm
+	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("Spring Arm"));
+	SpringArm->SetupAttachment(RootComponent);
+	SpringArm->TargetArmLength = 350.0f;
+	SpringArm->bUsePawnControlRotation = true;
+
+	// Create camera
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(SpringArm);
+}
+
+// Called when the game starts or when spawned
+void ABasePlayer::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABasePlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+void ABasePlayer::MoveForward(float InputValue)
+{
+	if (Controller != NULL && InputValue != 0)
+	{
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+	}
+}
+
+void ABasePlayer::MoveRight(float InputValue)
+{
+}
+
+void ABasePlayer::CameraLookUp(float InputValue)
+{
+}
+
+void ABasePlayer::CameraLookRight(float InputValue)
+{
+}
+
+// Called to bind functionality to input
+void ABasePlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
